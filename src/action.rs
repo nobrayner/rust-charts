7c2841aa32f153202992500da2b226e6119df32a
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-08-04 07:26:11 +1000

    feat: basic data shape for action

--- src/action.rs
+++ src/action.rs
@@ -1,2 +1,12 @@
-#[derive(Debug)]
-pub struct Action {}
+use std::{collections::HashMap, fmt};
+
+pub struct Action {
+  kind: String,
+  exec: Box<dyn Fn() -> ()>,
+  data: HashMap<String, String>, // <String, Any>, but not sure how that will go
+}
+impl fmt::Debug for Action {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    f.debug_struct("Action").field("type", &self.kind).finish()
+  }
+}
