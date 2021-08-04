faa8a3676c8fe8ab61e5103ef6d850b717cf9ed3
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-08-05 08:01:44 +1000

    feat: basic shape for event

--- src/event.rs
+++ src/event.rs
@@ -1,6 +1,15 @@
-pub struct Event {}
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Event {
+  name: String,
+  data: HashMap<String, String>,
+}
 impl Event {
   pub fn new() -> Self {
-    Event {}
+    Event {
+      name: String::from(""),
+      data: HashMap::new(),
+    }
   }
 }
